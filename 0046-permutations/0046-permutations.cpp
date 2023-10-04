8085f9e172275db942d2df6d76f270a83a906a90
Md Sohag Biswas <95488340+GodMischief@users.noreply.github.com>
2023-10-04 16:17:05 +0530

    Time: 4 ms (43.88%), Space: 8 MB (34.73%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -12,15 +12,12 @@ private:
             result.push_back(nums);
             return;
         }
-        // Generate permutations by swapping elements starting from the 'start' index
+        
         for (int i = start; i < nums.size(); ++i) {
             swap(nums[start], nums[i]);
-            // Recursively generate permutations for the remaining elements
             generatePermutations(nums, start + 1, result);
             swap(nums[start], nums[i]);
         }
     }
-// Code by ~ Md Sohag Biswas
+//Code by ~ Md Sohag Biswas
 };
-
-
