fcaa8d64cdcf9d2431cba0983f0a76f94992906b
Md Sohag Biswas <95488340+GodMischief@users.noreply.github.com>
2023-10-15 21:35:38 +0530

    Time: 23 ms (51.30%), Space: 14.2 MB (32.03%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -3,21 +3,18 @@ public:
     int numWays(int steps, int arrLen) {
         const int MOD = 1000000007;
         int n = min(arrLen, (steps / 2) + 1);
-        vector<long> dp(n, 0);
-        dp[0] = 1;
 
-        for (int step = 0; step < steps; ++step) {
-            vector<long> newDp(n, 0);
+        vector<vector<long>> dp(steps + 1, vector<long>(n, 0));
+        dp[0][0] = 1;
 
-            for (int i = 0; i < n; ++i) {
-                newDp[i] = dp[i];
-                if (i > 0) newDp[i] = (newDp[i] + dp[i - 1]) % MOD;
-                if (i < n - 1) newDp[i] = (newDp[i] + dp[i + 1]) % MOD;
+        for (int step = 1; step <= steps; ++step) {
+            for (int position = 0; position < n; ++position) {
+                dp[step][position] = dp[step - 1][position];
+                if (position > 0) dp[step][position] = (dp[step][position] + dp[step - 1][position - 1]) % MOD;
+                if (position < n - 1) dp[step][position] = (dp[step][position] + dp[step - 1][position + 1]) % MOD;
             }
-
-            dp = move(newDp);
         }
 
-        return dp[0];
+        return dp[steps][0];
     }
 };
