2e8a611a5003e2435fe4047eed3d1819e53e2826
Md Sohag Biswas <95488340+GodMischief@users.noreply.github.com>
2023-10-29 08:39:05 +0530

    Time: 2 ms (53.09%), Space: 6.7 MB (8.64%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int rounds = minutesToTest / minutesToDie + 1;
+        int pigs = 0;
+        
+        while (pow(rounds, pigs) < buckets) {
+            pigs++;
+        }
+        
+        return pigs;
+    }
+};
